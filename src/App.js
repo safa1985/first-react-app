5267c1c5ddf0858cf9805f79dc3356df8c260807
safa1985 <safa.khlaifat@gmail.com>
2024-05-12 19:22:16 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./componants/Title";
+import Image from "./componants/Image";
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <img src="https://royalph.com/pub/media/logo/stores/1/Artboard_1_2x.png" />
+      <Image />
     </div>
   );
 }
---
+++ src/componants/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Image = () => {
+  return (
+    <div>
+      <img src="https://royalph.com/pub/media/logo/stores/1/Artboard_1_2x.png" />
+    </div>
+  );
+};
+
+export default Image;
