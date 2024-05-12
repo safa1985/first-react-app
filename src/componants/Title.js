6f12c094e64ce81c5f5b78015bfb664b71512cd2
safa1985 <safa.khlaifat@gmail.com>
2024-05-12 19:04:21 +0300

    done

---
+++ src/componants/Title.js
@@ -0,0 +1,13 @@
+const Title = () => {
+  return (
+    <div>
+      <h1>
+        <strong> Royal Pharmacy</strong>
+      </h1>
+      <h3>
+        Royal Pharmacy Kuwait online store. Your first choice for your needs
+      </h3>
+    </div>
+  );
+};
+export default Title;
